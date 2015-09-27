8a766a21a9cc06785d22c8e5a325692c02824b11
Josh Lockhart <hello@joshlockhart.com>
2015-09-27 19:32:49 -0400

    Compress CSS output

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
             dist: {
                 options: {
                     cleancss: true,
+                    compress: true,
                     ieCompat: true
                 },
                 files: {
